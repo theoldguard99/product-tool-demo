7b61ed3d84465a52f4622eff71b81f51c450ad36
Darren Fadrilan <darrendfadrilan@gmail.com>
2024-06-09 21:31:22 +0800

    first initial push

---
+++ react-restaurant/src/App.js
@@ -0,0 +1,11 @@
+import LoginForm from './components/loginForm/LoginForm';
+
+function App() {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-restaurant/src/components/loginForm/LoginForm.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./LoginForm.css";
+import { FaUser, FaUnlock } from "react-icons/fa";
+
+const LoginForm = () => {
+  return (
+    <div className="wrapper">
+      <form action="">
+        <h1>Login</h1>
+        <div className="input-box">
+          <input type="text" placeholder="Username" required />
+          <FaUser className="icon" />
+        </div>
+        <div className="input-box">
+          <input type="passowrd" placeholder="Password" required />
+          <FaUnlock className="icon" />
+        </div>
+        <div className="remember-forgot">
+          <label>
+            <input type="checkbox" />
+            Remember me
+          </label>
+          <a href="#">forgot password?</a>
+        </div>
+        <button type="submit">Login</button>
+        <div className="register-link">
+          <p>
+            Don't have an account? <a href="">Register</a>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
