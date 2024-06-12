a1b4e9c2bfbe4a1823a6aed188528fca20d2be7f
Darren Fadrilan <darrendfadrilan@gmail.com>
2024-06-13 00:23:29 +0800

    feature-003 - create search and sort table

--- react-restaurant/src/components/Products/components/ProductsDetail/ProductsDetail.jsx
+++ react-restaurant/src/components/Products/components/ProductsDetail/ProductsDetail.jsx
@@ -2,12 +2,29 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { database } from "../../../../firebaseConfig";
 import { ref, get, update, remove } from "firebase/database";
+import {
+  Card,
+  CardHeader,
+  Divider,
+  TextField,
+  Grid,
+  Box,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  InputAdornment,
+} from "@mui/material";
+import { useSnackbar } from "notistack";
+import ImageInput from "../ProductsImage/ProductsImage";
+import SaveButtonBar from "../../../SaveButtonBar/SaveButtonBar";
 
 const ProductsDetail = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const [product, setProduct] = useState(null);
   const [isEditing, setIsEditing] = useState(false);
+  const { enqueueSnackbar } = useSnackbar();
 
   useEffect(() => {
     const productRef = ref(database, `merchant/products/${id}`);
@@ -32,29 +49,55 @@ const ProductsDetail = () => {
     }));
   };
 
+  const handleImageChange = (file) => {
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setProduct((prevProduct) => ({
+          ...prevProduct,
+          imageUrl: reader.result,
+        }));
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setProduct((prevProduct) => ({
+        ...prevProduct,
+        imageUrl: "",
+      }));
+    }
+  };
+
   const handleSave = () => {
     const productRef = ref(database, `merchant/products/${id}`);
     update(productRef, product)
       .then(() => {
-        alert("Product updated successfully!");
+        enqueueSnackbar("Product updated successfully!", {
+          variant: "success",
+        });
         setIsEditing(false);
+        navigate("/products");
       })
       .catch((error) => {
-        console.error("Error updating product:", error);
+        enqueueSnackbar("Error updating product: " + error.message, {
+          variant: "error",
+        });
       });
   };
 
   const handleDelete = () => {
-    // eslint-disable-next-line no-restricted-globals
-    if (confirm("Are you sure you want to delete this product?")) {
+    if (window.confirm("Are you sure you want to delete this product?")) {
       const productRef = ref(database, `merchant/products/${id}`);
       remove(productRef)
         .then(() => {
-          alert("Product deleted successfully!");
+          enqueueSnackbar("Product deleted successfully!", {
+            variant: "success",
+          });
           navigate("/products");
         })
         .catch((error) => {
-          console.error("Error deleting product:", error);
+          enqueueSnackbar("Error deleting product: " + error.message, {
+            variant: "error",
+          });
         });
     }
   };
@@ -63,109 +106,162 @@ const ProductsDetail = () => {
     return <div>Loading...</div>;
   }
 
+  const productCategories = [
+    { value: "Meat", label: "Meat" },
+    { value: "Poultry", label: "Poultry" },
+    { value: "Dairy and Produce", label: "Dairy and Produce" },
+    {
+      value: "Frozen Fruits and Vegetables",
+      label: "Frozen Fruits and Vegetables",
+    },
+    { value: "Dry Products", label: "Dry Products" },
+    {
+      value: "Non-Foods/Disposables/Smallwares",
+      label: "Non-Foods/Disposables/Smallwares",
+    },
+    { value: "Beverage", label: "Beverage" },
+  ];
+
+  const productSize =
+    product.category === "Non-Foods/Disposables/Smallwares"
+      ? [{ value: "None", label: "None" }]
+      : [
+          { value: "Small", label: "Small" },
+          { value: "Medium", label: "Medium" },
+          { value: "Large", label: "Large" },
+        ];
+
   return (
-    <div>
-      <h2>Product Details</h2>
-      <div>
-        <strong>Category:</strong>
-        {isEditing ? (
-          <input
-            type="text"
-            name="category"
-            value={product.category}
-            onChange={handleChange}
-          />
-        ) : (
-          <span> {product.category}</span>
-        )}
-      </div>
-      <div>
-        <strong>Name:</strong>
-        {isEditing ? (
-          <input
-            type="text"
-            name="name"
-            value={product.name}
-            onChange={handleChange}
-          />
-        ) : (
-          <span> {product.name}</span>
-        )}
-      </div>
-      <div>
-        <strong>Options:</strong>
-        {isEditing ? (
-          <input
-            type="text"
-            name="options"
-            value={product.options}
-            onChange={handleChange}
-          />
-        ) : (
-          <span> {product.options}</span>
-        )}
-      </div>
-      <div>
-        <strong>Price:</strong>
-        {isEditing ? (
-          <input
-            type="number"
-            name="price"
-            value={product.price}
-            onChange={handleChange}
-          />
-        ) : (
-          <span> {product.price}</span>
-        )}
-      </div>
-      <div>
-        <strong>Cost:</strong>
-        {isEditing ? (
-          <input
-            type="number"
-            name="cost"
-            value={product.cost}
-            onChange={handleChange}
-          />
-        ) : (
-          <span> {product.cost}</span>
-        )}
-      </div>
-      <div>
-        <strong>Amount in Stock:</strong>
-        {isEditing ? (
-          <input
-            type="number"
-            name="amountInStock"
-            value={product.amountInStock}
-            onChange={handleChange}
-          />
-        ) : (
-          <span> {product.amountInStock}</span>
-        )}
-      </div>
-      {product.imageUrl && (
-        <div>
-          <strong>Image:</strong>
-          <img
-            src={product.imageUrl}
-            alt={product.name}
-            style={{ maxWidth: "100%", height: "auto" }}
-          />
-        </div>
-      )}
-      {isEditing ? (
-        <>
-          <button onClick={handleSave}>Save</button>
-          <button onClick={() => setIsEditing(false)}>Cancel</button>
-        </>
-      ) : (
-        <button onClick={() => setIsEditing(true)}>Edit</button>
-      )}
-      <button onClick={handleDelete} style={{ marginLeft: "10px" }}>
-        Delete
-      </button>
-    </div>
+    <>
+      <Card className="card-container">
+        <CardHeader
+          title={<span className="bold-title">Product Details</span>}
+        />
+        <Divider fullWidth />
+        <Box p={2}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth margin="normal" disabled={!isEditing}>
+                <InputLabel id="category-label">Category</InputLabel>
+                <Select
+                  labelId="category-label"
+                  id="category"
+                  name="category"
+                  value={product.category}
+                  onChange={handleChange}
+                  label="Category"
+                >
+                  {productCategories.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                margin="normal"
+                type="text"
+                name="name"
+                label="Name"
+                value={product.name}
+                onChange={handleChange}
+                disabled={!isEditing}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FormControl
+                fullWidth
+                margin="normal"
+                disabled={
+                  !isEditing ||
+                  product.category === "Non-Foods/Disposables/Smallwares"
+                }
+              >
+                <InputLabel id="size-label">Size</InputLabel>
+                <Select
+                  labelId="size-label"
+                  id="options"
+                  name="options"
+                  value={product.options}
+                  onChange={handleChange}
+                  label="Size"
+                >
+                  {productSize.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                margin="normal"
+                type="number"
+                name="price"
+                label="Price"
+                value={product.price}
+                onChange={handleChange}
+                disabled={!isEditing}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">₱</InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                margin="normal"
+                type="number"
+                name="cost"
+                label="Cost"
+                value={product.cost}
+                onChange={handleChange}
+                disabled={!isEditing}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">₱</InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                margin="normal"
+                type="number"
+                name="amountInStock"
+                label="Amount in Stock"
+                value={product.amountInStock}
+                onChange={handleChange}
+                disabled={!isEditing}
+              />
+            </Grid>
+          </Grid>
+          <Box mt={2} display="flex" justifyContent="flex-end"></Box>
+        </Box>
+      </Card>
+      <ImageInput
+        onImageChange={handleImageChange}
+        imageUrl={product.imageUrl}
+      />
+      <SaveButtonBar
+        onSave={handleSave}
+        onEdit={() => setIsEditing(true)}
+        onCancel={() => {
+          setIsEditing(false);
+        }}
+        onDelete={handleDelete}
+        isEditing={isEditing}
+        hasDetails={!!product}
+      />
+    </>
   );
 };
 
--- react-restaurant/src/index.js
+++ react-restaurant/src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { SnackbarProvider } from 'notistack';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SnackbarProvider
+      maxSnack={3}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      autoHideDuration={2000}
+    >
+      <App />
+    </SnackbarProvider>
   </React.StrictMode>
 );
-
