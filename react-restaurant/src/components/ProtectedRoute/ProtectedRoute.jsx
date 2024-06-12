8c737fd7a64f24181643f710178a9132e8beea6a
Darren Fadrilan <darrendfadrilan@gmail.com>
2024-06-13 01:17:03 +0800

    feature-003 - remove logging

--- react-restaurant/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ react-restaurant/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ isLoggedIn, children }) => {
-  console.log("ProtectedRoute isLoggedIn:", isLoggedIn);
   return isLoggedIn ? children : <Navigate to="/login" />;
 };
 
